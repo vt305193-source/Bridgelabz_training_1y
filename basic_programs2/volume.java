fbcd5ad98e3ab88b627fecc5f5996e232d5c9a49
deepak tyagi <vt305193@gmail.com>
2026-01-19 10:17:33 +0530

    [deepak tyagi] remove file from mainbranch

--- basic_programs2/volume.java
+++
@@ -1,9 +0,0 @@
-class volume {
-    public static void main(String[] args) {
-        float r = 3;
-        float height = 10;
-        float volume = (22/7)*r*r*height ;
-        System.out.println("volume of cylinder:- " +volume);
-        
-    }
-}
